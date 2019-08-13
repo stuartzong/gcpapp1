6e92b715a14576ade55c8aa3488bb446507a9c3a
Stuart Zong <szong@bcgsc.ca>
2019-08-13 10:18:44 -0700

    fix datstore

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 import datetime
-
 from flask import Flask, render_template
 from google.cloud import datastore
 
 app = Flask(__name__)
 
 
-
 datastore_client = datastore.Client()
 
 def store_time(dt):
     entity = datastore.Entity(key=datastore_client.key('visit'))
-    entity.update({
-        'timestamp': dt
-    })
-
+    entity.update({'timestamp': dt})
     datastore_client.put(entity)
 
 
 def fetch_times(limit):
     query = datastore_client.query(kind='visit')
     query.order = ['-timestamp']
-
     times = query.fetch(limit=limit)
-
     return times
 
 
@@ -36,20 +29,7 @@ def root():
     # Fetch the most recent 10 access times from Datastore.
     times = fetch_times(10)
 
-    return render_template(
-        'index.html', times=times)
-
-
-#@app.route('/')
-#def root():
-#    # For the sake of example, use static information to inflate the template.
-#    # This will be replaced with real information in later steps.
-#    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-#                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-#                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-#                   ]
-#
-#    return render_template('index.html', times=dummy_times)
+    return render_template('index.html', times=times)
 
 
 if __name__ == '__main__':
